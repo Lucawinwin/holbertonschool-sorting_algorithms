a31929f031752004796e499f17a140d5578cd1a0
Lucawinwin <windwehrluca@gmail.com>
2024-12-14 18:31:44 +0100

    18h30

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,42 @@
 #include "sort.h"
-
-// Fonction de tri à bulle
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Échange les éléments
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble Sort algorithm
+ * @array: Pointer to the array of integers
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+        if (array == NULL || size < 2)
+        {
+        return;
         }
-    }
 
-return 0;
+        int temp;
+        int swapped;
+
+        for (size_t i = 0; i < size - 1; i++)
+        {
+        swapped = 0;
+
+        for (size_t j = 0; j < size - i - 1; j++)
+        {
+                if (array[j] > array[j + 1])
+                {
+
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                print_array(array, size);
+
+                swapped = 1;
+                }
+        }
+
+        if (!swapped)
+        {
+        break;
+        }
+        }
 }
